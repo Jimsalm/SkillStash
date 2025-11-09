6b08568160d2e39b48ba064a2c6505e1c26d46f6
Jimsalm <hypermood15.jimiel@gmail.com>
2025-11-09 13:43:43 +0800

    update: backend is now connected to the mongodb

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connectDB } from './config/db'; 
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001; // Use a different port than React (3000)
+
+connectDB();
+
+// Middlewares
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // Enable parsing of JSON bodies
+
+// A simple test route
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from the Express server!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
