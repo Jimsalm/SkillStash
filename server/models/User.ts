0e6377a4a491104e199061c6b6c8409144f5f1f0
SeigiMondragon <sonic92yeylol@gmail.com>
2025-11-25 19:45:22 +0800

    rEGISTER ENDPOINT

---
+++ server/models/User.ts
@@ -0,0 +1,30 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Input your name"],
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: [true, "Input your email"],
+    unique: true,
+    trim: true,
+  },
+
+  password: {
+    type: String,
+    required: [true, "Input your password"],
+    trim: true,
+  },
+});
+
+module.exports = mongoose.model<IUser>("User", UserSchema);
