ad88035ebd1c513ea04774cae14f548305e68aca
SeigiMondragon <sonic92yeylol@gmail.com>
2025-11-26 15:19:34 +0800

    handleRegister front end

--- client/src/pages/Auth/Register.tsx
+++ client/src/pages/Auth/Register.tsx
@@ -7,18 +7,39 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useState } from "react";
+import { authService } from "@/services/authService";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 const Register = () => {
+  const [email, setEmail] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPass, setConfirmPass] = useState<string>("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    if (password != confirmPass) {
+      return console.log("Password has to match");
+    }
+
+    const registeredData = await authService.registerUser({
+      name,
+      email,
+      password,
+    });
+    console.log(registeredData);
+  };
+
   return (
     <>
       <div className="flex justify-center items-center min-h-[100vh]">
         <Card className="flex flex-row w-full max-w-4xl p-4 space-x-6 ">
           <CardContent className="flex-[1]  "></CardContent>
-          <CardContent className="flex  flex-col justify-center  max-w-md w-full space-y-3 py-3">
+          <CardContent className="flex  flex-col justify-center max-w-md w-full space-y-3 py-3">
             <Label>Name</Label>
             <Input type="Text" placeholder="Name"></Input>
             <Label>Email</Label>
