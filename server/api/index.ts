7d5ecb6facd332bef00936db561b4b8abddf7ae8
Jimsalm <hypermood15.jimiel@gmail.com>
2025-12-27 14:37:54 +0800

    up

--- server/api/index.ts
+++ server/api/index.ts
@@ -24,7 +24,7 @@ app.use(
       if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
-        callback(new Error("Not allowed by CORS"));
+        callback(null, true);
       }
     },
     credentials: true
