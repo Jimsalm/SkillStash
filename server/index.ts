31329f1b565f127c4f01ede320c243604465ddba
Jimsalm <hypermood15.jimiel@gmail.com>
2025-12-27 16:37:18 +0800

    update

---
+++ server/index.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDB } from "./config/db";
+import coursesRouter from "./routes/course";
+import dashboardRouter from "./routes/dashboard";
+import authRouter from "./routes/auth";
+import reportsRouter from "./routes/reports";
+import serverless from "serverless-http";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(
+  cors({ 
+    origin: [
+      'http://localhost:5173',
+      'https://skillstash.vercel.app'
+    ],
+    credentials: true
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/api/courses", coursesRouter);
+app.use("/api/dashboard", dashboardRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/reports", reportsRouter);
+
+connectDB();
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+
+export default serverless(app);
