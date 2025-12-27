8717fc39e4a0be33448d80fbba288e5106cc65fc
Jimsalm <hypermood15.jimiel@gmail.com>
2025-12-27 12:23:53 +0800

    update: server part 3

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001', // Your Express server
+        target: 'http://localhost:5001',
         changeOrigin: true,
       },
     },
